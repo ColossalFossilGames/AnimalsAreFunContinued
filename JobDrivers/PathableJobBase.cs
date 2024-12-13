b30a3d2edc32bc32c91da5b8307da50168429463
David LeBlanc <email@david-leblanc.com>
2024-12-12 22:41:26 -0500

    Use the walking path from the JoyGiver rather than the JobDriver

--- JobDrivers/PathableJobBase.cs
+++ JobDrivers/PathableJobBase.cs
@@ -8,65 +8,12 @@ namespace AnimalsAreFunContinued.JobDrivers
 {
     public abstract class PathableJobBase : JobBase
     {
-        public List<LocalTargetInfo>? Path = null;
+        public List<LocalTargetInfo> Path = null!;
 
         public LocalTargetInfoDelegate CreateNextWaypointDelegate(bool preserveStack = false) => delegate ()
         {
             return PullNextWaypoint(preserveStack);
         };
-
-        public bool FindOutsideWalkingPath()
-        {
-            try
-            {
-                Pawn animal = job.GetTarget(TargetIndex.B).Pawn;
-                if (
-                    FindWalkingDestination(pawn, animal, out IntVec3 walkingDestination) &&
-                    WalkPathFinder.TryFindWalkPath(pawn, walkingDestination, out List<IntVec3> path)
-                )
-                {
-                    Path = new List<LocalTargetInfo>(path.Count);
-                    for (int pathIndex = 0; pathIndex < path.Count; pathIndex++)
-                    {
-                        Path.Add(path[pathIndex]);
-                    }
-                    return true;
-                }
-
-                Path = null;
-                return false;
-            }
-            catch (Exception ex)
-            {
-                AnimalsAreFunContinued.LogError($"An error occurred in FindOutsideWalkingPath(). {ex.Message}");
-                Path = null;
-                return false;
-            }
-        }
-
-        private static bool FindWalkingDestination(Pawn pawn, Pawn animal, out IntVec3 walkingDestination)
-        {
-            IntVec3 potentialDestination = new();
-            bool CellGoodForWalking(IntVec3 cell)
-            {
-                Map map = animal.MapHeld;
-                return !PawnUtility.KnownDangerAt(cell, map, pawn) &&
-                        !cell.GetTerrain(map).avoidWander &&
-                        cell.Standable(map) &&
-                        !cell.Roofed(map);
-            };
-
-            bool RegionGoodForWalking(Region region) =>
-                 region.Room.PsychologicallyOutdoors &&
-                 !region.IsForbiddenEntirely(animal) &&
-                 !region.IsForbiddenEntirely(pawn) &&
-                 region.TryFindRandomCellInRegionUnforbidden(animal, CellGoodForWalking, out potentialDestination) &&
-                 !potentialDestination.IsForbidden(pawn);
-
-            bool isValidDestination = CellFinder.TryFindClosestRegionWith(animal.GetRegion(), TraverseParms.For(animal), RegionGoodForWalking, 100, out _);
-            walkingDestination = potentialDestination;
-            return isValidDestination;
-        }
 
         private LocalTargetInfo PullNextWaypoint(bool preserveStack = false)
         {
