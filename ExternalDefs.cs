ef5910a7bab2c69dc1f05b73e04bf950655a77b1
David LeBlanc <email@david-leblanc.com>
2025-08-10 01:34:29 -0400

    Have animal wait for pawn first before pawn approaches

--- ExternalDefs.cs
+++ ExternalDefs.cs
@@ -9,6 +9,7 @@ namespace AnimalsAreFunContinued
         public static JobDef AAFC_WalkPet = null!;
         public static JobDef AAFC_PlayFetch = null!;
         public static JobDef AAFC_FetchItem = null!;
+        public static JobDef AAFC_WaitForPawn = null!;
 
         static ExternalDefs()
         {
